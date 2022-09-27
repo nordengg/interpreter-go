ed2da7a9693b0ac40eb734df8ace6ed32b11a653
nordengg <0xnord@protonmail.com>
2022-09-27 10:33:30 -0500

    added keyword identifier in token.go for check if given ident is a keyword

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -1,5 +1,17 @@
 package token
 
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 type TokenType string
 
 type Token struct {
