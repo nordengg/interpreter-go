5e309fdf6ad088c24445d96bf722926a009718af
nordengg <0xnord@protonmail.com>
2022-09-26 21:23:36 -0500

    created test for lexer

---
+++ src/monkey/lexer/lexer_test.go
@@ -0,0 +1,35 @@
+package lexer
+
+import (
+	"testing"
+
+	"monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+	}
+}
