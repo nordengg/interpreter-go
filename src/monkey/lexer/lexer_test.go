abf48c6ebb5acbb4253312af6065367b1fb13aa4
nordengg <0xnord@protonmail.com>
2022-09-29 10:58:48 -0500

    extended lexer test case to include new keywords and two-character operators

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -15,6 +15,17 @@ func TestNextToken(t *testing.T) {
 			  };
 			  
 			  let result = add(five, ten);
+			  !-/*5;
+			  5 < 10 > 5;
+
+			  if (5 < 10) {
+				return true;
+			  } else {
+				return false;
+			  }
+
+			  10 == 10;
+			  10 != 9;
 			 `
 
 	tests := []struct {
@@ -56,6 +67,42 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.IDENT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IDENT, "5"},
+		{token.LT, "<"},
+		{token.IDENT, "10"},
+		{token.GT, ">"},
+		{token.IDENT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.IDENT, "5"},
+		{token.LT, "<"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"}
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.IDENT, "10"},
+		{token.EQ, "=="},
+		{token.IDENT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.IDENT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.IDENT, "9"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
