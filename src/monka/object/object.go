8370e657edf2c414518c1a7b1a012cad004df7d9
nordengg <0xnord@protonmail.com>
2022-10-16 23:25:35 -0500

    added object system

---
+++ src/monka/object/object.go
@@ -0,0 +1,34 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+type Object interface {
+  Type() ObjectType
+  Inspect() string
+}
+
+type Integer struct {
+  Value int64
+}
+
+type Boolean struct {
+  Value bool
+}
+
+type Null struct {}
+
+const (
+  INTEGER_OBJ = "INTEGER"
+  BOOLEAN_OBJ = "BOOLEAN"
+  NULL_OBJ = "NULL"
+)
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string { return "null" }
