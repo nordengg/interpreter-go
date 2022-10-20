b5804a5b6664d0472b0d67b9ba37893db2ab6987
nordengg <0xnord@protonmail.com>
2022-10-20 16:33:38 -0500

    started hash implementation

--- src/monka/object/object.go
+++ src/monka/object/object.go
@@ -5,6 +5,7 @@ import (
   "bytes"
   "monka/ast"
   "strings"
+  "hash/fnv"
 )
 
 type ObjectType string
@@ -47,6 +48,24 @@ type Array struct {
   Elements []Object
 }
 
+type HashKey struct {
+  Type  ObjectType
+  Value uint64
+}
+
+type HashPair struct {
+  Key   Object
+  Value Object
+}
+
+type Hash struct {
+  Pairs map[HashKey]HashPair
+}
+
+type Hashable interface {
+  HashKey() HashKey
+}
+
 type Error struct {
   Message string
 }
@@ -61,6 +80,7 @@ const (
   STRING_OBJ = "STRING"
   BUILTIN_OBJ = "BUILTIN"
   ARRAY_OBJ = "ARRAY"
+  HASH_OBJ = "HASH"
 )
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
@@ -115,6 +135,45 @@ func (ao *Array) Inspect() string {
   out.WriteString("[")
   out.WriteString(strings.Join(elements, ", "))
   out.WriteString("]")
+
+  return out.String()
+}
+
+func (b *Boolean) HashKey() HashKey {
+  var value uint64
+
+  if b.Value {
+    value = 1
+  } else {
+    value = 0
+  }
+
+  return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+  return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+  h := fnv.New64a()
+  h.Write([]byte(s.Value))
+
+  return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+  var out bytes.Buffer
+
+  pairs := []string{}
+  for _, pair := range h.Pairs {
+    pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+  }
+
+  out.WriteString("{")
+  out.WriteString(strings.Join(pairs, ", "))
+  out.WriteString("}")
 
   return out.String()
 }
