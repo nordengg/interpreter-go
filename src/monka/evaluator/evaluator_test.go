4c1d2c564b7ccc0a606ebd7ae5c9abdd9c9d9bcc
nordengg <0xnord@protonmail.com>
2022-10-21 14:12:57 -0500

    finished evaluation implementation for hashs

--- src/monka/evaluator/evaluator_test.go
+++ src/monka/evaluator/evaluator_test.go
@@ -234,6 +234,10 @@ func TestErrorHandling(t *testing.T) {
       `"Hello" - "World"`,
       "unknown operator: STRING - STRING",
     },
+    {
+      `{"name": "Monka"}[fn(x) { x }];`,
+      "unusable as hash key: FUNCTION",
+    },
   }
 
   for _, tt := range tests {
@@ -386,8 +390,8 @@ func TestHashLiterals(t *testing.T) {
   {
     "one": 10 - 9,
     two: 1 + 1,
-    "thr" + "ee": 6 / 2
-    4: 4
+    "thr" + "ee": 6 / 2,
+    4: 4,
     true: 5,
     false: 6
   }`
@@ -418,6 +422,52 @@ func TestHashLiterals(t *testing.T) {
     }
 
     testIntegerObject(t, pair.Value, expectedValue)
+  }
+}
+
+func TestHashIndexExpressions(t *testing.T) {
+  tests := []struct {
+    input    string
+    expected interface{}
+  }{
+    {
+      `{"foo": 5}["foo"]`,
+      5,
+    },
+    {
+      `{"foo": 5}["bar"]`,
+      nil,
+    },
+    {
+      `let key = "foo"; {"foo": 5}[key]`,
+      5,
+    },
+    {
+      `{}["foo"]`,
+      nil,
+    },
+    {
+      `{5: 5}[5]`,
+      5,
+    },
+    {
+      `{true: 5}[true]`,
+      5,
+    },
+    {
+      `{false: 5}[false]`,
+      5,
+    },
+  }
+
+  for _, tt := range tests {
+    evaluated := testEval(tt.input)
+    integer, ok := tt.expected.(int)
+    if ok {
+      testIntegerObject(t, evaluated, int64(integer))
+    } else {
+      testNullObject(t, evaluated)
+    }
   }
 }
 
